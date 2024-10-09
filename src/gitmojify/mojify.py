84dc035df9efea125440a6e800076c356026c939
ljnsn <info@ljnsn.com>
2024-10-10 00:39:35 +0200

    ✨ feat(gitmojify): add extra arguments and implement conversion

--- src/gitmojify/mojify.py
+++ src/gitmojify/mojify.py
@@ -2,7 +2,9 @@ import argparse
 import re
 import sys
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List
+
+from commitizen import config
 
 from shared.model import Gitmoji
 from shared.utils import get_gitmojis, get_pattern
@@ -14,8 +16,24 @@ def get_args() -> argparse.Namespace:
     """Parse command line arguments."""
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-f", "--commit-msg-file")
-    group.add_argument("-m", "--message")
+    group.add_argument(
+        "-f",
+        "--commit-msg-file",
+        help="path to the commit message file",
+    )
+    group.add_argument("-m", "--message", help="the commit message")
+    parser.add_argument("--config", help="path to the configuration file")
+    parser.add_argument(
+        "--allowed-prefixes",
+        nargs="*",
+        help="allowed gitmoji prefixes",
+    )
+    parser.add_argument(
+        "--convert",
+        action="store_true",
+        default=False,
+        help="convert allowed prefixes to gitmoji format",
+    )
     return parser.parse_args()
 
 
@@ -24,7 +42,7 @@ def grouped_gitmojis() -> Dict[str, Gitmoji]:
     return {moji.type: moji for moji in get_gitmojis()}
 
 
-def gitmojify(message: str) -> str:
+def gitmojify(message: str, allowed_prefixes: List[str], *, convert: bool) -> str:
     """
     Gitmojify the commit message.
 
@@ -34,10 +52,20 @@ def gitmojify(message: str) -> str:
 
     Args:
         message: The complete commit message.
+        allowed_prefixes: Prefixes that should not raise an error, even though
+            they're not following conventional standard.
+        convert: Whether to convert the allowed prefixes to gitmoji format.
 
     Returns:
         The gitmojified message.
     """
+    if any(map(message.startswith, allowed_prefixes)):
+        if convert:
+            first_word, *rest = message.split()
+            message = f"{first_word.lower()}: {' '.join(rest)}"
+        else:
+            return message
+
     pat = re.compile(get_pattern())
     match = pat.match(message)
     if match is None:
@@ -52,6 +80,8 @@ def gitmojify(message: str) -> str:
 def run() -> None:
     """The pre-commit hook that modifies the commit message."""
     args = get_args()
+    cz_cfg = config.read_cfg(args.config)
+    allowed_prefixes = args.allowed_prefixes or cz_cfg.settings["allowed_prefixes"]
     if args.commit_msg_file:
         filepath = Path(args.commit_msg_file)
         # TODO: commit message file encoding can be set via git config
@@ -59,7 +89,7 @@ def run() -> None:
         # from there.
         msg = filepath.read_text(encoding=UTF8)
         with filepath.open("w", encoding=UTF8) as f:
-            f.write(gitmojify(msg))
+            f.write(gitmojify(msg, allowed_prefixes, convert=args.convert))
     else:
         msg = args.message
-        sys.stdout.write(gitmojify(msg))
+        sys.stdout.write(gitmojify(msg, allowed_prefixes, convert=args.convert))
