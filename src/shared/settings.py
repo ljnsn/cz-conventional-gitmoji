20257680be44c0e66ec0dd2d3a81c8b4faea68b2
ljnsn <info@ljnsn.com>
2024-10-11 23:04:13 +0200

    🐛 fix(shared): keep only config fields needed

--- src/shared/settings.py
+++ src/shared/settings.py
@@ -1,10 +1,9 @@
 """Settings."""
 
-from typing import Any, Dict, List, Optional, Tuple
+from typing import List, Optional
 
 import attrs
 
-from commitizen.defaults import CzSettings
 from commitizen import config
 
 DEFAULT_CONVERT_PREFIXES = ["Merge", "Revert", "Squash"]
@@ -14,35 +13,8 @@ DEFAULT_CONVERT_PREFIXES = ["Merge", "Revert", "Squash"]
 class MojiSettings:
     """Settings."""
 
-    name: str
-    version: Optional[str]
-    version_files: List[str]
-    version_provider: Optional[str]
-    version_scheme: Optional[str]
-    version_type: Optional[str] = None
-    tag_format: str
-    bump_message: Optional[str]
-    retry_after_failure: bool
-    allow_abort: bool
     allowed_prefixes: List[str]
     convert_prefixes: List[str] = attrs.field(factory=lambda: DEFAULT_CONVERT_PREFIXES)
-    changelog_file: str
-    changelog_format: Optional[str]
-    changelog_incremental: bool
-    changelog_start_rev: Optional[str]
-    changelog_merge_prerelease: bool
-    update_changelog_on_bump: bool
-    use_shortcuts: bool
-    style: Optional[List[Tuple[str, str]]] = None
-    customize: Optional[CzSettings] = None
-    major_version_zero: bool
-    pre_bump_hooks: Optional[List[str]]
-    post_bump_hooks: Optional[List[str]]
-    prerelease_offset: int
-    encoding: str
-    always_signoff: bool
-    template: Optional[str]
-    extras: Dict[str, Any]
     conventional_types_only: bool = False
     conventional_messages: bool = False
 
@@ -50,4 +22,5 @@ class MojiSettings:
 def get_settings(filepath: Optional[str] = None) -> MojiSettings:
     """Get settings."""
     cfg = config.read_cfg(filepath)
-    return MojiSettings(**cfg.settings)
+    fields_used = {field.name for field in attrs.fields(MojiSettings)}
+    return MojiSettings(**{k: v for k, v in cfg.settings.items() if k in fields_used})  # type: ignore[arg-type]
